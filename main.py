35e46fefd17a3f6b9a010ffbe370c436fa1d662a
Ritwik Suneliya <ritwiksuneliya@gmail.com>
2026-01-15 22:17:25 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,58 @@
+from fastapi import FastAPI, Request, Form, Depends
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import RedirectResponse, HTMLResponse
+from sqlalchemy import create_engine, Column, Integer, String, Boolean
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, Session
+
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+
+# Database setup
+SQLALCHEMY_DATABASE_URL = "sqlite:///./todos.db"
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+class Todo(Base):
+    __tablename__ = "todos"
+    id = Column(Integer, primary_key=True, index=True)
+    task = Column(String, index=True)
+    completed = Column(Boolean, default=False)
+
+Base.metadata.create_all(bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@app.get("/", response_class=HTMLResponse)
+async def read_todos(request: Request, db: Session = Depends(get_db)):
+    todos = db.query(Todo).all()
+    return templates.TemplateResponse("index.html", {"request": request, "todos": todos})
+
+@app.post("/create")
+async def create_todo(task: str = Form(...), db: Session = Depends(get_db)):
+    todo = Todo(task=task)
+    db.add(todo)
+    db.commit()
+    return RedirectResponse(url="/", status_code=303)
+
+@app.post("/update/{todo_id}")
+async def update_todo(todo_id: int, completed: bool = Form(False), db: Session = Depends(get_db)):
+    todo = db.query(Todo).filter(Todo.id == todo_id).first()
+    if todo:
+        todo.completed = completed
+        db.commit()
+    return RedirectResponse(url="/", status_code=303)
+
+@app.post("/delete/{todo_id}")
+async def delete_todo(todo_id: int, db: Session = Depends(get_db)):
+    todo = db.query(Todo).filter(Todo.id == todo_id).first()
+    if todo:
+        db.delete(todo)
+        db.commit()
+    return RedirectResponse(url="/", status_code=303)
